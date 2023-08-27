dd17d6de2f88c78e65b829ebbd2efdffe6640e0e
mohamedkhedr700 <M.khedr@rowaad.net>
2023-08-28 02:27:44 +0300

    Enhance helpers

--- src/Helpers/event-helper.php
+++ src/Helpers/event-helper.php
@@ -2,21 +2,21 @@
 
 use Raid\Core\Events\Contracts\EventableInterface;
 use Raid\Core\Events\Contracts\EventManagerInterface;
-use Raid\Core\Events\EventManager;
+use Raid\Core\Facades\Events\Event;
 
 if (! function_exists('events')) {
     /**
-     * Get repository events manager.
+     * Get event manager instance.
      */
     function events(): EventManagerInterface
     {
-        return EventManager::getFacadeRoot();
+        return Event::getFacadeRoot();
     }
 }
 
 if (! function_exists('eventable')) {
     /**
-     * Get eventable manager.
+     * Get eventable instance.
      */
     function eventable(string $eventable, string $action = ''): EventableInterface
     {
