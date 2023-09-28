6d8b67cb7fd3553af94e6c191f09d1ad09027011
mohamedkhedr700 <M.khedr@rowaad.net>
2023-09-29 01:47:07 +0300

    Add create event command

--- src/Commands/PublishCommand.php
+++ src/Commands/PublishCommand.php
@@ -8,7 +8,7 @@ class PublishCommand extends CorePublishCommand
     /**
      * {@inheritdoc}
      */
-    protected $name = 'core:event publish';
+    protected $name = 'core:publish-event';
 
     /**
      * {@inheritdoc}
